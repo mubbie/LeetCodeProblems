d372c91199ce89ff4dc747db288d06d4337153da
Mubarak Idoko <66747577+mubbie@users.noreply.github.com>
2022-11-14 20:46:42 -0500

    Longest Palindromic Substring

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,14 @@
+# Problem: https://leetcode.com/problems/longest-palindromic-substring/
+# Leetcode MEDIUM
+
+
+# THINKING:
+
+
+# SOLUTION
+class Solution:
+    @staticmethod
+    def longestPalindrome(s: str) -> str:
+
+
+# TESTING
